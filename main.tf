3365d2a4c6b782f9c560cd61f4407418a1b03540
Indigo_cE9 <chrisyeohc@outlook.com>
2025-04-05 11:22:24 +0800

    Adding new s3 bucket

--- main.tf
+++ main.tf
@@ -3,10 +3,12 @@ provider "aws" {
 }
 
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
-    aws = {
+       aws = {
       source  = "hashicorp/aws"
       version = "5.83.1"
+  
     }
   }
 }
