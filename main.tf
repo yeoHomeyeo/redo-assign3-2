278c86064717ac66b2ec9ec3ff86da00381961d9
Indigo_cE9 <chrisyeohc@outlook.com>
2025-04-05 11:17:49 +0800

    Adding new s3 bucket

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
 terraform {
   backend "s3" {
     bucket = "sctp-ce9-tfstate"
-    key    = "chrisy-s3-tf-ci.tfstate"  #Change this
+    key    = "chrisy-s3-tf-ci.tfstate" #Change this
     region = "us-east-1"
   }
 }
@@ -23,8 +23,8 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}"
-  account_id  = "${data.aws_caller_identity.current.account_id}"
+  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
+  account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
