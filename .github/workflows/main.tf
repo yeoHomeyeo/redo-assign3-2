d387d1a4061902660866657ef38069134cbca69b
Indigo_cE9 <chrisyeohc@outlook.com>
2025-04-05 09:49:16 +0800

    Adding new s3 bucket

---
+++ .github/workflows/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce9-tfstate"
+    key    = "chrisy-s3-tf-ci.tfstate"  #Change this
+    region = "us-east-1"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}"
+  account_id  = "${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+}
